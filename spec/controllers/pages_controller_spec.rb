89a5bd9c6131c3cf17880a9cc25ae7837452729e
nesmishlenish <nesmishlenish@gmail.com>
2013-03-17 13:43:57 +0300

    New Help page added

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,11 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before :each do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  # Rspec test for Home page
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -12,10 +17,25 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Home")
+                        :content => @base_title + " | Home")
     end
   end
 
+  # Rspec test for Help page
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                        :content => @base_title + " | Help")
+    end
+  end
+
+  # Rspec test for Contact page
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
@@ -25,10 +45,11 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Contact")
+                        :content => @base_title + " | Contact")
     end
   end
 
+  # Rspec test for About page
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
@@ -37,8 +58,8 @@ describe PagesController do
     
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | About")
+      response.should have_selector("title", 
+                        :content => @base_title + " | About")
     end
   end
 
